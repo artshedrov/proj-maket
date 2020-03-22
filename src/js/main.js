e8838340c8b854aeb058a27a04f834caac4ab7a2
artschedrov <art.worknsk@gmail.com>
2020-03-22 21:36:13 +0700

    22.03.20 add sliders

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+let projectSlider = new Swiper('.project__slider', {
+    slidesPerView: 2,
+    spaceBetween: 10,
+    // pagination: {
+    //   el: '.swiper-pagination',
+    //   clickable: true,
+    // },
+    navigation: {
+        nextEl: '.project__slider__btn--next',
+        prevEl: '.project__slider__btn--prev'
+      }
+  });
+
+  let arendaSlider = new Swiper ('.arenda__slider', {
+      slidesPerView: 1,
+      spaceBetween: 60,
+      navigation: {
+          nextEl: '.arenda__slider__btn--next',
+          prevEl: '.arenda__slider__btn--prev'
+      }
+  })
